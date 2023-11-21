87bf493a00346a19e1c7926fc98558a0a36e7b51
nestibry <nestibry@gmail.com>
2023-11-21 15:43:06 -0600

    added reactionSchema and reactionCount to thought schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,22 +1,12 @@
 const mongoose = require("mongoose");
+const reactionSchema = require("./Reaction");
 
 /*
- Thought:
-    Using timestamps: true to get the createdAt value
-    Need to come back and use a getter method to format the timestamp on query
-    createdAt: 'Jun 10th, 2020 at 07:30'
+ Notes:
     Using 'user' instead of username to pass in the user's _id, and from there use joins to get the username
-    Still need to link up to a reactionSchema
+    Using timestamps: true to get the createdAt value
+    Need to come back and use a getter method to format the timestamp on query => createdAt: 'Jun 10th, 2020 at 07:30'
 */
-
-
-// reactions (These are like replies)
-
-// Array of nested documents created with the reactionSchema
-// Schema Settings:
-
-// Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
-
 
 const thoughtSchema = new mongoose.Schema(
     {
@@ -31,12 +21,20 @@ const thoughtSchema = new mongoose.Schema(
             ref: "User",
             require: true
         }, 
+        reactions: [reactionSchema],
 
     },
     {
-        timestamps: true
+        timestamps: true,
+        toJSON: {
+            getters: true,
+          },
     }
 );
+
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
 
 const Thought = mongoose.model("Thought", thoughtSchema);
 module.exports = Thought;
