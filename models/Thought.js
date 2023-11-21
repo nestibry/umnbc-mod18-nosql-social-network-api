6d5e587a3b0368e9975be881cdee70947d11cd58
nestibry <nestibry@gmail.com>
2023-11-21 15:56:15 -0600

    init Thought virtual created_on to be used to format the createdAt...this may or may not work as intended, circle back to it leater

--- models/Thought.js
+++ models/Thought.js
@@ -6,6 +6,7 @@ const reactionSchema = require("./Reaction");
     Using 'user' instead of username to pass in the user's _id, and from there use joins to get the username
     Using timestamps: true to get the createdAt value
     Need to come back and use a getter method to format the timestamp on query => createdAt: 'Jun 10th, 2020 at 07:30'
+    get: (date) => `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()} at ${date.toLocaleTimeString()}`
 */
 
 const thoughtSchema = new mongoose.Schema(
@@ -36,5 +37,8 @@ thoughtSchema.virtual('reactionCount').get(function() {
     return this.reactions.length;
 });
 
+thoughtSchema.virtual('created_on').get(function() {
+    return this.createdAt;
+})
 const Thought = mongoose.model("Thought", thoughtSchema);
 module.exports = Thought;
